e7b2cd145d9716c1efc2cc9dfc78d2699711c8d4
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-11-01 07:46:13 -0300

    Criado filtro e busca na página de equipamentos

--- src/pages/EquipsPage.js
+++ src/pages/EquipsPage.js
@@ -1,26 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import JsonWeapons from "../info/weapons.json";
-import { Navbar, Footer } from "../globals/globals"; // Caminho atualizado
+import { Navbar, Footer } from "../globals/globals";
 
 function EquipsPage() {
+  const [searchText, setSearchText] = useState("");
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [filtersActive, setFiltersActive] = useState(false);
+
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+    setFiltersActive(true);
+  };
+
+  const handleTagSelect = (tag) => {
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(
+        selectedTags.filter((selectedTag) => selectedTag !== tag)
+      );
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+    setFiltersActive(true);
+  };
+
+  const clearFilters = () => {
+    setSearchText("");
+    setSelectedTags([]);
+    setFiltersActive(false);
+  };
+
+  const filteredWeapons = JsonWeapons.filter((weapon) => {
+    const nameMatch = weapon.name
+      .toLowerCase()
+      .includes(searchText.toLowerCase());
+    const tagsMatch =
+      selectedTags.length === 0 ||
+      selectedTags.every((tag) => weapon.tags.includes(tag));
+    return nameMatch && tagsMatch;
+  });
+
   return (
     <div>
       <Navbar />
       <h1>Página de EquipsPage</h1>
       <section className="section">
         <div className="box">
+          {/* Campo de pesquisa */}
+          <input
+            type="text"
+            placeholder="Buscar por nome"
+            value={searchText}
+            onChange={handleSearchTextChange}
+          />
+
+          {/* Botões de filtro de tags */}
+          <div>
+            <button
+              onClick={() => handleTagSelect("tag1")}
+              style={{
+                backgroundColor: selectedTags.includes("tag1")
+                  ? "blue"
+                  : "gray",
+                color: selectedTags.includes("tag1") ? "white" : "black",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                marginRight: "5px",
+              }}
+            >
+              Tag 1
+            </button>
+            <button
+              onClick={() => handleTagSelect("tag2")}
+              style={{
+                backgroundColor: selectedTags.includes("tag2")
+                  ? "blue"
+                  : "gray",
+                color: selectedTags.includes("tag2") ? "white" : "black",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                marginRight: "5px",
+              }}
+            >
+              Tag 2
+            </button>
+
+            {/* Adicione botões para outras tags, se necessário */}
+          </div>
+
+          {/* Botão para limpar filtros */}
+          {filtersActive && (
+            <button onClick={clearFilters}>Limpar Filtros</button>
+          )}
+
           <div className="columns is-multiline">
-            {JsonWeapons.map((json, index) => (
+            {filteredWeapons.map((json, index) => (
               <div className="column is-one-quarter" key={index}>
-                <div className="card equips-card"> {/* Aplicando uma classe ao card */}
+                <div className="card equips-card">
                   <header className="card-header">
                     <p className="card-header-title">{json.name}</p>
                   </header>
                   <div className="card-content">
                     <div className="content">
                       O atributo de acerto é {json.atrAcerto}
-                      <br></br>
-                      O atributo Para Bonus de dano é {json.atrBonus}
+                      <br></br>O atributo Para Bonus de dano é {json.atrBonus}
                       <br></br>
                       Lançamento: {json.throw}
                       <br></br>
@@ -40,6 +122,5 @@ function EquipsPage() {
     </div>
   );
 }
-
 
 export default EquipsPage;
