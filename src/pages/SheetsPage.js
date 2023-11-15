073664d9d1491932992f898dc966f58e4d2955b4
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-11-15 13:09:48 -0300

    página de fichas

--- src/pages/SheetsPage.js
+++ src/pages/SheetsPage.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import JsonNpcs from "../info/npcs.json";
 import { Navbar, Footer } from "../globals/globals";
 
-
 function Tags({ tags }) {
   return (
     <div>
@@ -55,131 +54,132 @@ function SheetsPage() {
   return (
     <div>
       <Navbar></Navbar>
-      <br></br>
-      {/* Campo de pesquisa */}
-      <input
-        type="text"
-        placeholder="Buscar por nome"
-        value={searchText}
-        onChange={handleSearchTextChange}
-      />
-
-      <div>
-        <br></br>
-        {/* Começo de um botão de filtro de tags */}
-        <button
-          onClick={() => handleTagSelect("padrao")}
-          style={{
-            backgroundColor: selectedTags.includes("padrao") ? "green" : "blue",
-            color: selectedTags.includes("padrao") ? "white" : "white",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-            marginRight: "5px",
-          }}
-        >
-          <br></br>
-          Padrão
-        </button>
-        {/* fim de um botão de filtro de tags */}
-        <button
-          onClick={() => handleTagSelect("medieval")}
-          style={{
-            backgroundColor: selectedTags.includes("medieval")
-              ? "green"
-              : "blue",
-            color: selectedTags.includes("medieval") ? "white" : "white",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-            marginRight: "5px",
-          }}
-        >
-          <br></br>
-          Medieval
-        </button>
-      </div>
-
-      {/* Botão para limpar filtros */}
-      {filtersActive && (
-        <div>
-          <button onClick={clearFilters}>Limpar Filtros</button>
-          <br></br>
-        </div>
-      )}
-      <br></br>
 
       <section className="section">
         <div className="box">
-          <div className="columns is-multiline">
-          {filteredNpcs.map((json, index) => (
-            <div className="column is-half" key={index}>
-              <div className="box">
-                Nome do personagem {json.name} {json.efeito} {json.tags}
-                <br></br>
-                Dado de vida:
-                <br></br>
-                HP:
-                <br></br>
-                Mana:
-                <br></br>
-                atributo de Spellcasting:
-                <br></br>
-                Deslocamento:
-                <br></br>
-                Armadura:
-                <br></br>
-                Defesa:
-                <br></br>
-                Absorção:
-                <br></br>
-                Penalidade de movimentação:
-                <br></br>
-                <table class="table is-bordered is-striped is-hoverable">
-                  <thead>
-                    <tr>
-                      <th colSpan={2}>Atributos</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Força 1d20</td>
-                      <td>Destreza 1d20</td>
-                    </tr>
-                    <tr>
-                      <td>Inteligência 1d20</td>
-                      <td>Conhecimento 1d20</td>
-                    </tr>
-                    <tr>
-                      <td>Sabedoria 1d20</td>
-                      <td>Constituição 1d20</td>
-                    </tr>
-                    <tr>
-                      <td>Percepção 1d20</td>
-                      <td>Carisma 1d20</td>
-                    </tr>
-                    <tr>
-                      <td>Armas de uma mão 1d20</td>
-                      <td>Armas de duas mãos 1d20</td>
-                    </tr>
-                    <tr>
-                      <td>Furtividade 1d20</td>
-                      <td>Pontaria 1d20</td>
-                    </tr>
-                  </tbody>
-                </table>
-                Vantagens/perks
-                <br></br>
-                Spelllist
-                <br></br>
-                Armas
-                <br></br>
-                inventário
-                <br></br>
-                backstory
-              </div>
-            </div>        
-                ))}
-            {/* fim do card examplo*/}
+          <br></br>
+          {/* Campo de pesquisa */}
+          <input
+            type="text"
+            placeholder="Buscar por nome"
+            value={searchText}
+            onChange={handleSearchTextChange}
+          />
 
+          <div>
+            <br></br>
+            {/* Começo de um botão de filtro de tags */}
+            <button
+              onClick={() => handleTagSelect("padrao")}
+              style={{
+                backgroundColor: selectedTags.includes("padrao")
+                  ? "green"
+                  : "blue",
+                color: selectedTags.includes("padrao") ? "white" : "white",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                marginRight: "5px",
+              }}
+            >
+              <br></br>
+              Padrão
+            </button>
+            {/* fim de um botão de filtro de tags */}
+            <button
+              onClick={() => handleTagSelect("medieval")}
+              style={{
+                backgroundColor: selectedTags.includes("medieval")
+                  ? "green"
+                  : "blue",
+                color: selectedTags.includes("medieval") ? "white" : "white",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                marginRight: "5px",
+              }}
+            >
+              <br></br>
+              Medieval
+            </button>
+          </div>
+
+          {/* Botão para limpar filtros */}
+          {filtersActive && (
+            <div>
+              <button onClick={clearFilters}>Limpar Filtros</button>
+              <br></br>
+            </div>
+          )}
+          <br></br>
+          <div className="columns is-multiline">
+            {filteredNpcs.map((json, index) => (
+              <div className="column is-half" key={index}>
+                <div className="box">
+                  Nome do personagem {json.name} 
+                  <br></br>
+                  Dado de vida: {json.lifeDice} 
+                  <br></br>
+                  HP: {json.hp} 
+                  <br></br>
+                  Mana: {json.mp} 
+                  <br></br>
+                  atributo de Spellcasting: {json.atrSpell} 
+                  <br></br>
+                  Deslocamento: {json.desloc} 
+                  <br></br>
+                  Armadura: {json.armor} 
+                  <br></br>
+                  Defesa: {json.armDef} 
+                  <br></br>
+                  Absorção: {json.armAbsor} 
+                  <br></br>
+                  Penalidade de movimentação: {json.movPenalt} 
+                  <br></br>
+                  <table class="table is-bordered is-striped is-hoverable">
+                    <thead>
+                      <tr>
+                        <th colSpan={2}>Atributos</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Força: {json.for} </td>
+                        <td>Destreza: {json.des} </td>
+                      </tr>
+                      <tr>
+                        <td>Inteligência: {json.int} </td>
+                        <td>Conhecimento: {json.conh} </td>
+                      </tr>
+                      <tr>
+                        <td>Sabedoria: {json.sab} </td>
+                        <td>Constituição: {json.con} </td>
+                      </tr>
+                      <tr>
+                        <td>Percepção: {json.perc} </td>
+                        <td>Carisma: {json.cari} </td>
+                      </tr>
+                      <tr>
+                        <td>Armas de uma mão: {json.oneH} </td>
+                        <td>Armas de duas mãos: {json.twoH} </td>
+                      </tr>
+                      <tr>
+                        <td>Furtividade: {json.furt} </td>
+                        <td>Pontaria: {json.pont} </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  Vantagens/perks {json.perk} 
+                  <br></br>
+                  Spelllist {json.spellList} 
+                  <br></br>
+                  Armas {json.weapons} 
+                  <br></br>
+                  inventário {json.inventory} 
+                  <br></br>
+                  backstory {json.backstory} 
+                </div>
+              </div>
+            ))}
+            {/* fim do card examplo*/}
           </div>
         </div>
       </section>
