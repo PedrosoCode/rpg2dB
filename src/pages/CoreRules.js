966ba6d5517a3cf97930627f7962e7d98126178d
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-11-23 22:30:15 -0300

    Minor text fixes

--- src/pages/CoreRules.js
+++ src/pages/CoreRules.js
@@ -11,7 +11,8 @@ function CoreRules() {
       <section className="section is-medium daRulesHead">
         <h1 className="headtext">Regras de jogo</h1>
         <h2 className="headtext">
-         Todos os <strong>principais pontos </strong> para compreender a lógica de jogo
+          Todos os <strong>principais pontos </strong> para compreender a lógica
+          de jogo
         </h2>
         <div className="overlay"></div>
       </section>
@@ -26,23 +27,44 @@ function CoreRules() {
             <div class="content is-medium">
               <h1>Como São feitas as Rolagens</h1>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                aliquet lobortis lectus ut volutpat. Etiam nunc elit, gravida id
-                leo vitae, commodo tempus massa. Duis eget turpis in dui
-                dapibus.
+                Para você fazer as rolagens, vocẽ une seu dado primário (1D20)
+                que é imutável (salvo em raras excessões), com o seu dado
+                secundário do atributo/habilidade específica
               </p>
             </div>
             <div class="content is-medium">
               <h1>Atributos Principais</h1>
-              <h4>O que são Atributos?</h4>
-              <h4>Atributos do corpo</h4>
-              <h4>Atributos da mente</h4>
-              <h4>Atributos de Persistência</h4>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                aliquet lobortis lectus ut volutpat. Etiam nunc elit, gravida id
-                leo vitae, commodo tempus massa. Duis eget turpis in dui
-                dapibus.
+                Os Atributos principais são o ponto fundamental da criação do
+                personagem, pois a maior parte das builds e outros modificadores
+                irão depender desses Atributos
+              </p>
+              <li>Atributos Derivados</li>
+              <p>
+                Os Atributos derivados são aqueles que unicamente dependem de um
+                atributo principal
+              </p>
+              <h2>Os atributos podem ser separados nas seguintes categorias</h2>
+              <h4>Atributos do corpo</h4>
+              <li>Força</li>
+              <li>Destreza</li>
+              <h4>Atributos da mente</h4>
+              <li>Inteligência</li>
+              <li>sabedoria</li>
+              <li>Conhecimento</li>
+              <li>Percepção</li>
+              <h4>Atributos de Persistência</h4>
+              <li>Carisma</li>
+              <li>Constituição</li>
+              <h4>Atributos de Combate</h4>
+              <li>Armas de uma mão</li>
+              <li>Armas de duas Mãos</li>
+              <li>Pontaria</li>
+              <li>Furtividade</li>
+              <p>
+                Cada Atributo é governado por um rank (verificar tabela na home)
+                <br></br> Tal rank dita o dado respectivo a ser usado, bem como
+                o modificador que será incluído na rolagem
               </p>
               <h6>Modificadores</h6>
             </div>
@@ -151,7 +173,8 @@ function CoreRules() {
             </div>
           </div>
 
-          <MasterMenu /><br></br>
+          <MasterMenu />
+          <br></br>
         </div>
       </section>
 
